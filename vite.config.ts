57fcbcf21d459dee21392706b7115b72e0f1eba4
USSNCC-1701 <ussncc170stfc@gmail.com>
2026-03-15 19:26:20 -0400

    Second commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'USSNCC-1701/WebDev2', // Replace <your-repo-name> with your actual GitHub repository name
+  base: 'WebDev2', // Replace <your-repo-name> with your actual GitHub repository name
 });
