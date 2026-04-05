b8edb041aaffca54071d73014864477faf34401d
USSNCC-1701 <ussncc170stfc@gmail.com>
2026-04-05 15:58:35 -0400

    Added Form

--- src/MyNav.tsx
+++ src/MyNav.tsx
@@ -2,6 +2,7 @@
 import Home from "./Home";
 import About from "./About";
 import Products from "./Products";
+import MyForm from "./MyForm";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function MyNav() {
@@ -12,6 +13,7 @@ function MyNav() {
                 <Route path="/home" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/products" element={<Products />} />
+                <Route path="/form" element={<MyForm />} />
             </Routes>
         </BrowserRouter>
     );
@@ -24,6 +26,7 @@ function SetNav() {
                 <li><Link to="/home">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/products">Products</Link></li>
+                <li><Link to="/form">Form</Link></li>
             </ul>
         </nav>
     );
